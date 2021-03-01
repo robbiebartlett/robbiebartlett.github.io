522fa24920b6f4edf824b89df04f36b20061c548
Rob Bartlett <robbiebartlett@outlook.com>
2021-03-01 14:09:17 +1300

    navigation update

--- js/actions.js
+++ js/actions.js
@@ -1,29 +1,29 @@
 //Mobile menu
 function mobilemenu() {
-    $(".mobile-button").click(function() {
-        $(".mobile-menu").toggleClass("open");
-        $(".mobile-links").toggleClass("visible");
-      }),
-      
-      $(".anchor-link").click(function(){
-         $(".mobile-links").removeClass("visible");
-         $(".mobile-menu").removeClass("open");
-         $(".mobile-button").removeClass("is-active");
-      })
-      
-      var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
-            var hamburgers = document.querySelectorAll(".mobile-button");
-            if (hamburgers.length > 0) {
-              forEach(hamburgers, function(hamburger) {
-                hamburger.addEventListener("click", function() {
-                  this.classList.toggle("is-active");
-                }, false);
-              });
-            }
+		
+		$(".mobile-button").click(function() {
+			$(".mobile-menu").toggleClass("open");
+			$(".rb-navigation__list").toggleClass("visible");
+		}),
+		
+		$(".anchor-link").click(function(){
+			 $(".rb-navigation__list").removeClass("visible");
+			 $(".mobile-menu").removeClass("open");
+			 $(".mobile-button").removeClass("is-active");
+		})
+
+		
+		var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+					var hamburgers = document.querySelectorAll(".mobile-button");
+					if (hamburgers.length > 0) {
+						forEach(hamburgers, function(hamburger) {
+							hamburger.addEventListener("click", function() {
+								this.classList.toggle("is-active");
+							}, false);
+						});
+					}
+
 } 
-
-
-
 
 
 //Defer Youtube load
@@ -386,8 +386,8 @@ function copyright() {
 
 //Document - load
 $(document).ready(function () {
-	   mobilemenu(),
-	        defer(),
+       mobilemenu(),
+	    defer(),
          timeline(),
           shuffle(),
          lazyload(),
